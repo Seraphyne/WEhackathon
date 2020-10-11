d52b241982cca8f622040c4ffd304e67d7ead6d9
Anusha Saha <44592491+anushasaha16@users.noreply.github.com>
2020-10-10 23:41:09 -0500

    Add files via upload

---
+++ SearchScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, View } from "react-native";
+
+function SearchScreen(props) {
+    return (
+        <View></View>
+    );
+}
+
+const styles = StyleSheet.create({
+    
+})
+
+export default SearchScreen;
